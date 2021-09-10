f765c4b90619e389b0bfb946c0f9f79433c51caa
oguzkahraman <oguz.kahraman@optdcom.net>
2021-09-10 22:26:51 +0300

    Fixed some errors

--- src/main/java/com/io/fastmeet/controllers/impl/CalendarControllerImpl.java
+++ src/main/java/com/io/fastmeet/controllers/impl/CalendarControllerImpl.java
@@ -1,29 +1,29 @@
-/*
- * @author : Oguz Kahraman
- * @since : 4.08.2021
- *
- * Copyright - Calendar App Java API
- **/
-package com.io.fastmeet.controllers.impl;
-
-import com.io.fastmeet.controllers.CalendarController;
-import com.io.fastmeet.models.requests.calendar.CalendarTypeCreateRequest;
-import com.io.fastmeet.services.CalendarService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-
-@RestController
-public class CalendarControllerImpl implements CalendarController {
-
-    @Autowired
-    private CalendarService calendarService;
-
-    @Override
-    public ResponseEntity<Void> createCalendarType(@Valid CalendarTypeCreateRequest request, String token) {
-        return null;
-    }
-
-}
+///*
+// * @author : Oguz Kahraman
+// * @since : 4.08.2021
+// *
+// * Copyright - Calendar App Java API
+// **/
+//package com.io.fastmeet.controllers.impl;
+//
+//import com.io.fastmeet.controllers.CalendarController;
+//import com.io.fastmeet.models.requests.calendar.CalendarTypeCreateRequest;
+//import com.io.fastmeet.services.CalendarService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.validation.Valid;
+//
+//@RestController
+//public class CalendarControllerImpl implements CalendarController {
+//
+//    @Autowired
+//    private CalendarService calendarService;
+//
+//    @Override
+//    public ResponseEntity<Void> createCalendarType(@Valid CalendarTypeCreateRequest request, String token) {
+//        return null;
+//    }
+//
+//}
