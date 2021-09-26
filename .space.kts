1722a25fd428fb2a414b5f7fd8a3bc7859e7a9dd
oguzkahraman <oguz.kahraman@optdcom.net>
2021-09-26 22:39:30 +0300

    Renamed package and project

--- .space.kts
+++ .space.kts
@@ -1,9 +1,19 @@
-job("Build and run tests") { 
-   container("maven:3.6.3-openjdk-16-slim") { 
-       shellScript { 
-           content = """ 
+job("Build and run tests") {
+    container("maven:3.6.3-openjdk-16") {
+        shellScript {
+            content = """ 
 	            mvn clean install 
-           """ 
-       } 
-   } 
+           """
+        }
+    }
+}
+
+job("Deploy Heroku") {
+    container("maven:3.6.3-openjdk-16") {
+        shellScript {
+            content = """ 
+	            mvn heroku:deploy -DskipTests=true 
+           """
+        }
+    }
 }
