db6e7702e7904600a635ce19fb5109bc535fe3ca
ayaz <doguhanayaz@gmail.com>
2021-10-04 09:38:05 +0200

    Controller Annototation added. ToDo update method in Service.

--- src/main/java/com/io/collige/models/requests/todo/TodoUpdateRequest.java
+++ src/main/java/com/io/collige/models/requests/todo/TodoUpdateRequest.java
@@ -2,9 +2,15 @@ package com.io.collige.models.requests.todo;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class TodoUpdateRequest {
 
     private Long id;
+
+    private LocalDateTime updatedTime;
+
+    private String description;
 
 }
