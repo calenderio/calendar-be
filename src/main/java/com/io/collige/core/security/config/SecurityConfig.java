a37bdd33334d231a44cf7b779523c69b9dde6183
ogzkhrmn <ogz.khrmn52@gmail.com>
2021-10-07 23:17:50 +0300

    Added new tests

--- src/main/java/com/io/collige/core/security/config/SecurityConfig.java
+++ src/main/java/com/io/collige/core/security/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and()
+        http.cors().and().csrf().disable()
                 .authorizeRequests().antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
