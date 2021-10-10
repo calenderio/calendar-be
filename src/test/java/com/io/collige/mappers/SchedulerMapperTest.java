b46ad7afd8a73bc3960a9e14e0b530f295d1805e
Oguz Kahraman <oguzkahraman52@gmail.com>
2021-10-10 03:37:15 +0300

    Added mapper testes
    Signed-off-by: Oguz Kahraman <oguzkahraman52@gmail.com>

---
+++ src/test/java/com/io/collige/mappers/SchedulerMapperTest.java
@@ -0,0 +1,31 @@
+/*
+ * @author : Oguz Kahraman
+ * @since : 10.10.2021
+ *
+ * Copyright - Collige App Java API
+ **/
+package com.io.collige.mappers;
+
+import com.io.collige.models.internals.SchedulerDetails;
+import com.io.collige.models.internals.SchedulerObject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(MockitoExtension.class)
+class SchedulerMapperTest {
+
+    private final SchedulerMapper mapper = Mappers.getMapper(SchedulerMapper.class);
+
+    @Test
+    void mapToObject() {
+        SchedulerObject object = new SchedulerObject();
+        object.setSchedule(new SchedulerDetails());
+        mapper.mapToObject(object);
+        assertNotNull(object.getSchedule().getFri());
+    }
+
+}
