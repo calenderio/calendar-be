9070d3da4b505ad3ae2ecb4c27cce43fa7c7727e
doguhanayaz <doguhanayaz@gmail.com>
2021-10-11 22:59:19 +0200

    Anotation for TodoControllerImpl.java added

--- src/main/java/com/io/collige/controllers/impl/TodoControllerImpl.java
+++ src/main/java/com/io/collige/controllers/impl/TodoControllerImpl.java
@@ -10,9 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 public class TodoControllerImpl implements TodoController {
 
     @Autowired
