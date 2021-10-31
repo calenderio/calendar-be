c431e15f8179b0403337f986645967f20498cdfc
Oguz Kahraman <oguzkahraman52@gmail.com>
2021-10-31 21:31:16 +0300

    Added new test
    Signed-off-by: Oguz Kahraman <oguzkahraman52@gmail.com>

--- src/main/resources/db/migration/V1_1__craete_linked_calendars.sql
+++ src/main/resources/db/migration/V1_1__craete_linked_calendars.sql
@@ -11,7 +11,7 @@ create table linked_calendars
 );
 
 create
-    unique index linked_calendars_social_mail_uindex
+    index linked_calendars_social_mail_uindex
     on linked_calendars (social_mail);
 
 
---
+++ src/main/resources/db/migration/V_1_18__add_uindex_calendar.sql
@@ -0,0 +1,3 @@
+create
+    index linked_calendars_social_mail_uindex
+    on linked_calendars (social_mail, type);
